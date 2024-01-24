4e8fe2566371466f52cc54ebf58a8db157f0cec5
Thevindu Dilmith <thevindu.20210197@iit.ac.lk>
2024-01-24 09:48:37 +0530

    Update Products.java

--- WestminsterShoppingManager/src/Products.java
+++ WestminsterShoppingManager/src/Products.java
@@ -5,4 +5,13 @@ public abstract class Products implements Serializable {
     private String productName;
     private int numOfItems;
     private int price;
+
+    public String toString() {
+        return "Products{" +
+                "productId='" + productId + '\'' +
+                ", productName='" + productName + '\'' +
+                ", numOfItems=" + numOfItems +
+                ", price=" + price +
+                '}';
+    }
 }
