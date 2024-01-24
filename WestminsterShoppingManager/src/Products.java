b634839ded7b586527f108167123ede026910e5d
Thevindu Dilmith <thevindu.20210197@iit.ac.lk>
2024-01-24 09:50:14 +0530

    getProductId/setProductId

--- WestminsterShoppingManager/src/Products.java
+++ WestminsterShoppingManager/src/Products.java
@@ -14,4 +14,12 @@ public abstract class Products implements Serializable {
                 ", price=" + price +
                 '}';
     }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
 }
