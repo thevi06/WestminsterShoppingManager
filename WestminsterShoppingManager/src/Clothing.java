bc1ca6143e662d243456c52e8f224498f045f15b
Thevindu Dilmith <thevindu.20210197@iit.ac.lk>
2024-01-31 12:22:05 +0530

    create a constructor to initialize size & color

--- WestminsterShoppingManager/src/Clothing.java
+++ WestminsterShoppingManager/src/Clothing.java
@@ -1,4 +1,9 @@
 public class Clothing extends Products {
     private String size;
     private String color;
+
+    public Clothing(String size, String color) {
+        this.size = size;
+        this.color = color;
+    }
 }
