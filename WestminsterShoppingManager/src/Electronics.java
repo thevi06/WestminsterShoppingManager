4e2bce4f41e42628bf85b0e1c83fb0385449ce36
Thevindu Dilmith <thevindu.20210197@iit.ac.lk>
2024-02-06 12:05:51 +0530

    Update Electronics.java

--- WestminsterShoppingManager/src/Electronics.java
+++ WestminsterShoppingManager/src/Electronics.java
@@ -4,3 +4,11 @@ public class Electronics extends Products implements Serializable {
     private String brand;
     private int warrantyPeriod;
     private String name;//for demo
+
+    public Electronics(String brand, int warrantyPeriod, String name) {
+        this.brand = brand;
+        this.warrantyPeriod = warrantyPeriod;
+        this.name = name;
+    }
+}
+
