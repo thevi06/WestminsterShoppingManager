d80b835813e89ec8b9ae0a51d7675d4b82ac376d
Thevindu Dilmith <thevindu.20210197@iit.ac.lk>
2024-02-08 12:39:33 +0530

    Update ConsoleMenu.java

--- WestminsterShoppingManager/src/ConsoleMenu.java
+++ WestminsterShoppingManager/src/ConsoleMenu.java
@@ -1,2 +1,6 @@
+import java.util.Scanner;
+
 public class ConsoleMenu {
-}
+
+    static ShoppingManager manager = new WestminsterShoppingManager();
+    final static Scanner scn =new Scanner(System.in);
