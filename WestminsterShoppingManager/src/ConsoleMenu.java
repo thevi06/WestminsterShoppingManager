85060a3910be02b808d620cb1f6dfe2d9731f578
Thevindu Dilmith <thevindu.20210197@iit.ac.lk>
2024-02-08 12:40:04 +0530

    Update ConsoleMenu.java

--- WestminsterShoppingManager/src/ConsoleMenu.java
+++ WestminsterShoppingManager/src/ConsoleMenu.java
@@ -1,6 +1,15 @@
+import java.io.IOException;
 import java.util.Scanner;
 
 public class ConsoleMenu {
 
     static ShoppingManager manager = new WestminsterShoppingManager();
     final static Scanner scn =new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        menuloop:
+        while (true){
+            try {
+                displayMenu();
+                int choice = scn.nextInt();
+                //TODO: Validate the user input for integer
