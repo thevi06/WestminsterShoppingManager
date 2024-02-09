ff18eeecf5587d47b109576c1a9bbe8e14b619c6
Thevindu Dilmith <thevindu.20210197@iit.ac.lk>
2024-02-09 09:02:25 +0530

    Update WestminsterShoppingManager.java

--- WestminsterShoppingManager/src/WestminsterShoppingManager.java
+++ WestminsterShoppingManager/src/WestminsterShoppingManager.java
@@ -1,6 +1,11 @@
 import java.util.ArrayList;
 
 public class WestminsterShoppingManager implements ShoppingManager {
-    public final static int maxProducts=50;
+    public final static int maxProducts = 50;
     public ArrayList<Electronics> electList = new ArrayList<>();
-    public ArrayList <Clothing> clothList = new ArrayList<>();
+    public ArrayList<Clothing> clothList = new ArrayList<>();
+
+    public ArrayList<Electronics> getElectList() {
+        return electList;
+    }
+}
