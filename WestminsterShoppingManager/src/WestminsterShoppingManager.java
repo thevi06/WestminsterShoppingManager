22bce9c9c757b3a3e1cc71be18bc2ea87b1b575d
Thevindu Dilmith <thevindu.20210197@iit.ac.lk>
2024-02-19 09:50:39 +0530

    Update WestminsterShoppingManager.java

--- WestminsterShoppingManager/src/WestminsterShoppingManager.java
+++ WestminsterShoppingManager/src/WestminsterShoppingManager.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class WestminsterShoppingManager implements ShoppingManager {
     public final static int maxProducts = 50;
@@ -18,6 +20,14 @@ public class WestminsterShoppingManager implements ShoppingManager {
         if(getElectList().isEmpty() && getClothList().isEmpty()){
             System.out.println("No products have added");
         }else{
+
+            Collections.sort(electList, new Comparator<Electronics>() {
+                @Override
+                public int compare(Electronics e1, Electronics e2) {
+                    return e1.getProductId().compareTo(e2.getProductId());
+                }
+            });
+        }
 
     }
 }
