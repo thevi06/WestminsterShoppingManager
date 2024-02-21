a236a76caf5c777909ab09ec43b522ded636a66f
Thevindu Dilmith <thevindu.20210197@iit.ac.lk>
2024-02-21 16:07:23 +0530

    Update WestminsterShoppingManager.java

--- WestminsterShoppingManager/src/WestminsterShoppingManager.java
+++ WestminsterShoppingManager/src/WestminsterShoppingManager.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.Iterator;
 
 public class WestminsterShoppingManager implements ShoppingManager {
     public final static int maxProducts = 50;
@@ -15,11 +16,11 @@ public class WestminsterShoppingManager implements ShoppingManager {
         return clothList;
     }
 
-    public void printProduct(){
+    public void printProduct() {
 
-        if(getElectList().isEmpty() && getClothList().isEmpty()){
+        if (getElectList().isEmpty() && getClothList().isEmpty()) {
             System.out.println("No products have added");
-        }else{
+        } else {
 
             Collections.sort(electList, new Comparator<Electronics>() {
                 @Override
@@ -35,22 +36,22 @@ public class WestminsterShoppingManager implements ShoppingManager {
                 }
             });
             System.out.println("\n*** List of all items ***");
-            for (Electronics electronics:electList) {
-                System.out.println("\nProduct Id :"+electronics.getProductId());
-                System.out.println("Product Name :"+electronics.getProductName());
-                System.out.println("Number of Items :"+electronics.getNumOfItems());
-                System.out.println("Price :"+electronics.getPrice());
-                System.out.println("Brand :"+electronics.getBrand());
-                System.out.println("Warranty period :"+electronics.getWarrantyPeriod());
+            for (Electronics electronics : electList) {
+                System.out.println("\nProduct Id :" + electronics.getProductId());
+                System.out.println("Product Name :" + electronics.getProductName());
+                System.out.println("Number of Items :" + electronics.getNumOfItems());
+                System.out.println("Price :" + electronics.getPrice());
+                System.out.println("Brand :" + electronics.getBrand());
+                System.out.println("Warranty period :" + electronics.getWarrantyPeriod());
             }
 
-            for (Clothing clothing:clothList) {
-                System.out.println("\nProduct Id :"+clothing.getProductId());
-                System.out.println("Product Name :"+clothing.getProductName());
-                System.out.println("Number of Items :"+clothing.getNumOfItems());
-                System.out.println("Price :"+clothing.getPrice());
-                System.out.println("Size :"+clothing.getSize());
-                System.out.println("Color :"+clothing.getColor());
+            for (Clothing clothing : clothList) {
+                System.out.println("\nProduct Id :" + clothing.getProductId());
+                System.out.println("Product Name :" + clothing.getProductName());
+                System.out.println("Number of Items :" + clothing.getNumOfItems());
+                System.out.println("Price :" + clothing.getPrice());
+                System.out.println("Size :" + clothing.getSize());
+                System.out.println("Color :" + clothing.getColor());
             }
         }
 
@@ -58,4 +59,15 @@ public class WestminsterShoppingManager implements ShoppingManager {
 
     public void deleteProduct(String productId) {
         boolean found = false;
+        Iterator<Electronics> electronicsIterator = electList.iterator();
+        while (electronicsIterator.hasNext()) {
+            Electronics electronics = electronicsIterator.next();
+            if (electronics.getProductId().equals(productId)) {
+                found = true;
+                electronicsIterator.remove();
+                System.out.println("\nRemoved electronic item");
+                break;
+            }
+        }
+    }
 }
