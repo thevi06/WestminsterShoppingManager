a7212b0953d526cf88b7f93e953937a43a00003e
Thevindu Dilmith <thevindu.20210197@iit.ac.lk>
2024-03-01 15:41:03 +0530

    Update WestminsterShoppingManager.java

--- WestminsterShoppingManager/src/WestminsterShoppingManager.java
+++ WestminsterShoppingManager/src/WestminsterShoppingManager.java
@@ -1,13 +1,10 @@
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.Iterator;
+import java.util.*;
 
 public class WestminsterShoppingManager implements ShoppingManager {
-    public final static int maxProducts = 50;
-    public ArrayList<Electronics> electList = new ArrayList<>();
-    public ArrayList<Clothing> clothList = new ArrayList<>();
+    public final static int maxProducts=50;
+    public ArrayList <Electronics> electList = new ArrayList<>();
+    public ArrayList <Clothing> clothList = new ArrayList<>();
 
     public ArrayList<Electronics> getElectList() {
         return electList;
@@ -17,11 +14,12 @@ public class WestminsterShoppingManager implements ShoppingManager {
         return clothList;
     }
 
-    public void printProduct() {
+    public void printProduct(){
 
-        if (getElectList().isEmpty() && getClothList().isEmpty()) {
+
+        if(getElectList().isEmpty() && getClothList().isEmpty()){
             System.out.println("No products have added");
-        } else {
+        }else{
 
             Collections.sort(electList, new Comparator<Electronics>() {
                 @Override
@@ -30,6 +28,8 @@ public class WestminsterShoppingManager implements ShoppingManager {
                 }
             });
 
+
+
             Collections.sort(clothList, new Comparator<Clothing>() {
                 @Override
                 public int compare(Clothing c1, Clothing c2) {
@@ -37,29 +37,30 @@ public class WestminsterShoppingManager implements ShoppingManager {
                 }
             });
             System.out.println("\n*** List of all items ***");
-            for (Electronics electronics : electList) {
-                System.out.println("\nProduct Id :" + electronics.getProductId());
-                System.out.println("Product Name :" + electronics.getProductName());
-                System.out.println("Number of Items :" + electronics.getNumOfItems());
-                System.out.println("Price :" + electronics.getPrice());
-                System.out.println("Brand :" + electronics.getBrand());
-                System.out.println("Warranty period :" + electronics.getWarrantyPeriod());
+            for (Electronics electronics:electList) {
+                System.out.println("\nProduct Id :"+electronics.getProductId());
+                System.out.println("Product Name :"+electronics.getProductName());
+                System.out.println("Number of Items :"+electronics.getNumOfItems());
+                System.out.println("Price :"+electronics.getPrice());
+                System.out.println("Brand :"+electronics.getBrand());
+                System.out.println("Warranty period :"+electronics.getWarrantyPeriod());
             }
 
-            for (Clothing clothing : clothList) {
-                System.out.println("\nProduct Id :" + clothing.getProductId());
-                System.out.println("Product Name :" + clothing.getProductName());
-                System.out.println("Number of Items :" + clothing.getNumOfItems());
-                System.out.println("Price :" + clothing.getPrice());
-                System.out.println("Size :" + clothing.getSize());
-                System.out.println("Color :" + clothing.getColor());
+            for (Clothing clothing:clothList) {
+                System.out.println("\nProduct Id :"+clothing.getProductId());
+                System.out.println("Product Name :"+clothing.getProductName());
+                System.out.println("Number of Items :"+clothing.getNumOfItems());
+                System.out.println("Price :"+clothing.getPrice());
+                System.out.println("Size :"+clothing.getSize());
+                System.out.println("Color :"+clothing.getColor());
             }
         }
     }
 
-    // Using Iterator for safe removal
     public void deleteProduct(String productId) {
         boolean found = false;
+
+        // Using Iterator for safe removal
         Iterator<Electronics> electronicsIterator = electList.iterator();
         while (electronicsIterator.hasNext()) {
             Electronics electronics = electronicsIterator.next();
@@ -113,7 +114,7 @@ public class WestminsterShoppingManager implements ShoppingManager {
     }
 
     // Helper method to check if a product ID exists in either list
-    private boolean productExists(String productId) {
+    private boolean  productExists(String productId) {
         for (Electronics electronics : getElectList()) {
             if (electronics.getProductId().equals(productId)) {
                 return true;
@@ -133,19 +134,20 @@ public class WestminsterShoppingManager implements ShoppingManager {
         ObjectOutputStream objout = new ObjectOutputStream(fout);
 
         Iterator it = electList.iterator();
-        while (it.hasNext()) {
+        while(it.hasNext()){
             Electronics eleNew = (Electronics) it.next();
             objout.writeObject(eleNew);
             System.out.println("data saved successfully");
         }
 
         Iterator itc = clothList.iterator();
-        while (itc.hasNext()) {
+        while(itc.hasNext()){
             Clothing cloNew = (Clothing) itc.next();
             objout.writeObject(cloNew);
             System.out.println("clothing data saved successfully");
         }
     }
+
 
     public void loadData() throws IOException {
         boolean dataLoaded = false;  // Flag to track if any data was loaded from the file
@@ -153,47 +155,43 @@ public class WestminsterShoppingManager implements ShoppingManager {
         try (FileInputStream fin = new FileInputStream("newText.txt");
              ObjectInputStream objin = new ObjectInputStream(fin)) {
 
-            try (FileInputStream fin = new FileInputStream("newText.txt");
-                 ObjectInputStream objin = new ObjectInputStream(fin)) {
+            while (true) {
+                try {
+                    Object obj = objin.readObject();
 
-                while (true) {
-                    try {
-                        Object obj = objin.readObject();
-
-                        if (obj instanceof Electronics) {
-                            Electronics electronics = (Electronics) obj;
-                            electList.add(electronics);
-                            System.out.println("Electronics data loaded from text file");
-                            dataLoaded = true;
-
-                        } else if (obj instanceof Clothing) {
-                            Clothing clothing = (Clothing) obj;
-                            clothList.add(clothing);
-                            System.out.println("Clothing data loaded from text file");
-                            dataLoaded = true;
-                        }
-
-                    } catch (EOFException e) {
-                        // This exception will be caught when there are no more objects to read
-                        break;
-
-                    } catch (ClassNotFoundException | IOException e) {
-                        e.printStackTrace();
-                        break;
+                    if (obj instanceof Electronics) {
+                        Electronics electronics = (Electronics) obj;
+                        electList.add(electronics);
+                        System.out.println("Electronics data loaded from text file");
+                        dataLoaded = true;
+                    } else if (obj instanceof Clothing) {
+                        Clothing clothing = (Clothing) obj;
+                        clothList.add(clothing);
+                        System.out.println("Clothing data loaded from text file");
+                        dataLoaded = true;
                     }
+
+                } catch (EOFException e) {
+                    // This exception will be caught when there are no more objects to read
+                    break;
+
+                } catch (ClassNotFoundException | IOException e) {
+                    e.printStackTrace();
+                    break;
                 }
-
-            } catch (IOException e) {
-                // Handle other IO exceptions
-                e.printStackTrace();
             }
 
-            // Check the flag and display a message if the file was empty
-            if (!dataLoaded) {
-                System.out.println("Text file is empty");
-            }
+        } catch (IOException e) {
+            // Handle other IO exceptions
+            e.printStackTrace();
         }
 
-
+        // Check the flag and display a message if the file was empty
+        if (!dataLoaded) {
+            System.out.println("Text file is empty");
+        }
     }
+
+
+
 }
