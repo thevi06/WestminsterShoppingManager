473161a0932885ada2960b3fa60e9f5e3f432e2c
Thevindu Dilmith <thevindu.20210197@iit.ac.lk>
2024-03-03 22:33:03 +0530

    Update User.java

--- WestminsterShoppingManager/src/User.java
+++ WestminsterShoppingManager/src/User.java
@@ -2,4 +2,16 @@ public class User {
     private String userName;
     private String password;
 
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public User(String userName, String password) {
+        this.userName = userName;
+        this.password = password;
+    }
 }
