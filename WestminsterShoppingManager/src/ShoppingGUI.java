220c06b990b5040fb028a5de08f0c349fee82488
Thevindu Dilmith <thevindu.20210197@iit.ac.lk>
2024-03-05 10:16:19 +0530

    Update ShoppingGUI.java

--- WestminsterShoppingManager/src/ShoppingGUI.java
+++ WestminsterShoppingManager/src/ShoppingGUI.java
@@ -1,2 +1,7 @@
-public class ShoppingGUI {
-}
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+
+public class ShoppingGUI extends JFrame {
+
+    private DefaultTableModel tableModel;
+    private JPanel productDetailsPanel;
