e005121b0d2e2e9800cacd4b6efbd675a4a1e322
Thevindu Dilmith <thevindu.20210197@iit.ac.lk>
2024-03-05 10:16:45 +0530

    Update ShoppingGUI.java

--- WestminsterShoppingManager/src/ShoppingGUI.java
+++ WestminsterShoppingManager/src/ShoppingGUI.java
@@ -5,3 +5,7 @@ public class ShoppingGUI extends JFrame {
 
     private DefaultTableModel tableModel;
     private JPanel productDetailsPanel;
+
+    public ShoppingGUI(WestminsterShoppingManager manager) {
+        setSize(1000, 600);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
